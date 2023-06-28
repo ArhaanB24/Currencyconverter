f1dc736520d7102a11444b7dd55977a9c4ae92e1
Arhaan Bhiwandkar <arhaanbhiwandkar@gmail.com>
2023-06-28 13:18:45 +0530

    modified the input list

--- currency converter/main.py
+++ currency converter/main.py
@@ -1,13 +1,17 @@
 from flask import render_template,Flask,request
 import requests
 import json
+from Countrydetails import countries
+
 
 app = Flask(__name__)
 
+country = countries.all_countries()
+namdict = country.currencies()
 url = f"http://api.exchangeratesapi.io/v1/latest?access_key=db0c18c12b283976e7e890f63120934a"
 response = requests.get(url)
 curr = json.loads(response.text)
-curr_lst = list(curr["rates"].keys())
+curr_lst = list(namdict.keys())
 @app.route("/",methods=["GET","POST"])
 def home():
     ans=0
@@ -15,7 +19,7 @@ def home():
     second = request.form.get("second")
     amount = request.form.get("amount")
     if first and second and amount:
-        ans =  (int(amount)*curr["rates"][second])//curr["rates"][first]
+        ans = (int(amount)*curr["rates"][namdict[second]])//curr["rates"][namdict[first]]
     return render_template("index.html",curr_lst=curr_lst,ans = ans)
 if __name__ == "__main__":
     app.run(debug=True)
