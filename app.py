5a958cb0a541812f92e6a9d51a14b64ac6577778
naitik-lodha <naitik.lodha139@nmims.edu.in>
2023-06-29 14:42:46 +0530

    Bug free code

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
 from flask import render_template,Flask,request
 import requests
 import json
-from Countrydetails import countries
-
 
 app = Flask(__name__)
 
-country = countries.all_countries()
-namdict = country.currencies()
 url = f"http://api.exchangeratesapi.io/v1/latest?access_key=db0c18c12b283976e7e890f63120934a"
 response = requests.get(url)
 curr = json.loads(response.text)
-curr_lst = list(namdict.keys())
+curr_lst = list(curr["rates"].keys())
 @app.route("/",methods=["GET","POST"])
 def home():
     ans=0
@@ -19,7 +15,7 @@ def home():
     second = request.form.get("second")
     amount = request.form.get("amount")
     if first and second and amount:
-        ans = (int(amount)*curr["rates"][namdict[second]])//curr["rates"][namdict[first]]
+        ans =  (int(amount)*curr["rates"][second])//curr["rates"][first]
     return render_template("index.html",curr_lst=curr_lst,ans = ans)
 if __name__ == "__main__":
     app.run(debug=True)
